4c045e92eee7987a6f8881f114f9b44adf535757
Eduard Verameyenka <eduardver2@gmail.com>
2017-12-02 14:52:44 +0000

    add file upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,9 +1,31 @@
 var express = require("express"),
     router  = express.Router(),
     middleware = require("../middleware"),
-    Campground = require("../models/campground");
+    Campground = require("../models/campground"),
+    multer = require('multer');
     
 var { isLoggedIn, checkUserCampground } = middleware; // destructuring assignment
+
+//Image upload conf
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter});
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'upcampinc', 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
     
 //INDEX
 router.get("/",function(req,res){
@@ -20,22 +42,23 @@ router.get("/new", isLoggedIn, function(req,res){
 });
 
 //CREATE NEW
-router.post("/", isLoggedIn, function(req,res){
-     // get data from form and add to campgrounds array
-  var author = {
-      id: req.user._id,
-      username: req.user.username || req.user.email
-  }
-  var newCampground = {name: req.body.name, image: req.body.image, description: req.body.description, author: author, cost: req.body.cost};
-  Campground.create(newCampground,function(err,newlyCampground){
-      if(err){
-          req.flash("error",err.message);
-          res.redirect("/campgrounds");
-      } else {
-          res.redirect("/campgrounds");
-      }
-  });
-    
+router.post("/", isLoggedIn, upload.single('image'), function(req,res){
+  cloudinary.uploader.upload(req.file.path, function(result) {
+      // add cloudinary url for the image to the campground object under image property
+      req.body.campground.image = result.secure_url;
+      // add author to campground
+      req.body.campground.author = {
+        id: req.user._id,
+        username: req.user.username
+      };
+      Campground.create(req.body.campground, function(err, campground) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        res.redirect('/campgrounds/' + campground.id);
+      });
+    });
 });
 
 //SHOW
@@ -63,16 +86,33 @@ router.get("/:id/edit", isLoggedIn, checkUserCampground, function(req,res){
 });
 
 //UPDATE 
-router.put("/:id", isLoggedIn, checkUserCampground, function(req,res){
-   var newData = {name: req.body.name, image: req.body.image, description: req.body.description, cost: req.body.cost, createdAt: Date.now()};
-   Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err,updatedCampground){
-       if(err){
-           req.flash("error", err.message);
-           res.redirect("back");
-       } else {
-           res.render("campgrounds/show", {campground: updatedCampground});
-       }
-   }); 
+router.put("/:id", isLoggedIn, checkUserCampground, upload.single('image'),  function(req,res){
+    if(req.file  ){
+      cloudinary.uploader.upload(req.file.path, function(result) {
+          // add cloudinary url for the image to the campground object under image property
+          req.body.campground.image = result.secure_url;
+          Campground.findByIdAndUpdate(req.params.id, {$set: req.body.campground}, function(err,updatedCampground){
+              if(err){
+                  req.flash("error", err.message);
+                  return res.redirect("back");
+              } 
+              req.flash("success","Successfully Updated!");
+              res.redirect("/campgrounds/" + updatedCampground._id);
+              
+          });
+        });  
+    } else {
+        Campground.findByIdAndUpdate(req.params.id, {$set: req.body.campground}, function(err,updatedCampground){
+          if(err){
+              req.flash("error", err.message);
+              return res.redirect("back");
+          } 
+          req.flash("success","Successfully Updated!");
+          res.redirect("/campgrounds/" + updatedCampground._id);
+          
+      });
+    }
+    
 });
 
 //DELETE
@@ -85,7 +125,7 @@ router.delete("/:id", isLoggedIn, checkUserCampground,  function(req,res){
         req.flash('error', 'Campground deleted!');
         res.redirect('/campgrounds');
     })
-})
+});
 
 
 module.exports = router;
