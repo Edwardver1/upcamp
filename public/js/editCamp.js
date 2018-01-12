7f31be5fa37306b498b2d7fa8fb1ba9514991599
Eduard Verameyenka <eduardver2@gmail.com>
2018-01-12 07:29:23 +0000

    set logo for camp with fixed positions

--- public/js/editCamp.js
+++ public/js/editCamp.js
@@ -8,19 +8,24 @@ $(function() {
     $('.logo-btn').click(function(e){
         e.preventDefault();
         var $logo = $(this).parent().parent().parent();
-        var $first = $(this).parent().parent().parent().siblings('div:first');
+        var $logoUrl = $logo.find("input").attr("value");
+        var $first = $(this).parent().parent().parent().siblings('div .col-logo');
+        var $firstUrl = $first.find("input").attr("value");
         
+        $first.removeClass("col-logo");
         $first.children().removeClass("logo");
         $first.find(".removeIcon").removeClass("hidden");
         $first.find(".logo-btn").removeClass("hidden");
         $first.find(".logo-text").addClass("hidden");
+        $first.find("input").attr("value",$logoUrl);
         
+        $logo.addClass("col-logo");
         $logo.children().addClass("logo");
         $logo.find(".removeIcon").addClass("hidden");
         $logo.find(".logo-btn").addClass("hidden");
         $logo.find(".logo-text").removeClass("hidden");
+        $logo.find("input").attr("value",$firstUrl);
         
-        $first.before($logo);
     });
     
     
